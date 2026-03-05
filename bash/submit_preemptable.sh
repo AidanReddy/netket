4bf23b54d7adb267ca820bba19888a1bf5b5b833
AidanReddy <aidanpatrickreddy@gmail.com>
2026-03-05 09:27:08 -0500

    syncing

--- bash/submit_preemptable.sh
+++ bash/submit_preemptable.sh
@@ -2,8 +2,11 @@
 
 # Job Flags
 #SBATCH -p mit_preemptable
+#SBATCH --requeue
 #SBATCH -c 4
 #SBATCH -G h100:1
 
 # Run your application
-.venv/bin/python python ../scripts/run_haldane_psiformer.py
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
+"${REPO_ROOT}/.venv/bin/python" "${REPO_ROOT}/scripts/run_haldane_psiformer.py"
